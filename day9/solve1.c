16c242525eabe274ec53800d0c8ccf9bde151840
Bosco G. G <juan.bosco@uca.es>
2021-12-09 10:50:54 +0100

    Day 9 part 1 solved

--- day9/solve1.c
+++ day9/solve1.c
@@ -1,7 +1,34 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <ctype.h>
 
 #include "input.h"
+
+int check(char *lp, char *l, char *ln, int i){
+
+ char c = l[i];
+ if(i>0){
+  if(l[i-1] <= c){
+   return 0;
+  }
+ }
+ if(isdigit(l[i+1])){
+  if(l[i+1] <= c){
+   return 0;
+  }
+ }
+ if(lp != NULL){
+  if(lp[i] <= c){
+   return 0;
+  }
+ }
+ if(ln != NULL){
+  if(ln[i] <= c){
+   return 0;
+  }
+ }
+
+ return 1;
+}
 
 int solve(){
 
@@ -9,6 +36,43 @@ int solve(){
  if(input_init(input)){
   return 1;
  }
+
+ int i;
+ char *lp;
+ char *l;
+ char *ln;
+
+ unsigned int sum;
+
+ int k;
+ k = 0;
+
+ sum = 0;
+ lp = NULL;
+ l = input->p;
+ input_skipline(input);
+ ln = input->p;
+ while(l != NULL){
+
+  for(i=0;isdigit(l[i]);i++){
+   if(check(lp, l, ln, i)){
+    sum += l[i] - '0' + 1;
+   }
+  }
+
+  lp = l;
+  l = ln;
+  input_skipline(input);
+  if(input_endq(input)){
+   /* last line */
+   ln = NULL;
+  }else{
+   ln = input->p;
+  }
+  k++;
+ }
+
+ printf("%d\n", sum);
 
  return 0;
 }
