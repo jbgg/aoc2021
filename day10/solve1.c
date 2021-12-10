1da0fce524efeb8bf82de0bd8a8cf523a92afa85
Bosco G. G <juan.bosco@uca.es>
2021-12-10 09:44:22 +0100

    Day 10 part 1 solved

--- day10/solve1.c
+++ day10/solve1.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <ctype.h>
 
 #include "input.h"
@@ -9,6 +10,81 @@ int solve(){
  if(input_init(input)){
   return 1;
  }
+
+ int nlinemax;
+ int n;
+ char c;
+
+ nlinemax = 0;
+ while(!input_endq(input)){
+  n = 0;
+  while(!input_eol(input)){
+   c = input_readchar(input);
+   if( c!='(' && c!=')' &&
+     c!='[' && c!=']' &&
+     c!='{' && c!='}' &&
+     c!='<' && c!='>'){
+    return 1;
+   }
+   n++;
+  }
+  if(n > nlinemax){
+   nlinemax = n;
+  }
+  input_skipline(input);
+ }
+
+ input_init(input);
+
+ int i;
+ char *l;
+
+ l = malloc(sizeof(char)*(nlinemax+1));
+ if(l == NULL){
+  return 1;
+ }
+
+ int points;
+
+ n = 0;
+ points = 0;
+ while(!input_endq(input)){
+  i = 0;
+  while(!input_eol(input)){
+   c = input_readchar(input);
+   if( c=='(' || c=='[' || c=='{' || c=='<'){
+    l[i] = c;
+    i++;
+   }else{
+    if(i>0 &&
+      ((c==')' && l[i-1]=='(') ||
+       (c==']' && l[i-1]=='[') ||
+       (c=='}' && l[i-1]=='{') ||
+       (c=='>' && l[i-1]=='<'))){
+     i--;
+    }else{
+    /* illegal character */
+     if(c==')'){
+      points += 3;
+     }
+     if(c==']'){
+      points += 57;
+     }
+     if(c=='}'){
+      points += 1197;
+     }
+     if(c=='>'){
+      points += 25137;
+     }
+     break;
+    }
+   }
+  }
+  input_skipline(input);
+  n++;
+ }
+
+ printf("%d\n", points);
 
  return 0;
 }
