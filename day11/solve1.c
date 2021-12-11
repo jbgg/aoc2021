831df9b31c9e6f5c15e55f851711ed4b279c45bd
Bosco G. G <juan.bosco@uca.es>
2021-12-11 13:07:55 +0100

    Day 11 part 1 solved

--- day11/solve1.c
+++ day11/solve1.c
@@ -4,22 +4,37 @@
 
 #include "input.h"
 
+#define FLASHED (-1)
+
 /* energy levels */
 struct _e_t {
  int ni;
  int nj;
- unsigned int **d;
+ int **d;
 };
 typedef struct _e_t e_t;
 
 int e_init(e_t *p);
+int e_print(e_t p);
+int e_step(e_t *p, unsigned int *n);
+int e_incresearound(e_t *p, int ci, int cj);
 
 int solve(){
 
  e_t e;
+ int step;
+ unsigned int n;
+ unsigned int count;
+
  if(e_init(&e)){
   return 1;
  }
+ count = 0;
+ for(step=0;step<100;step++){
+  e_step(&e, &n);
+  count += n;
+ }
+ printf("%d\n", count);
 
  return 0;
 }
@@ -28,6 +43,8 @@ int e_init(e_t *p){
  int ni;
  int nj;
  int i;
+ int j;
+ int c;
 
  if(p == NULL){
   return 1;
@@ -59,7 +76,123 @@ int e_init(e_t *p){
    return 1;
   }
  }
- /* TODO */
+ input_init(input);
+ i = 0;
+ while(!input_endq(input)){
+  if(i>=ni){
+   return 1;
+  }
+  j = 0;
+  while(!input_eol(input)){
+   if(j>=nj){
+    return 1;
+   }
+   c = input_readchar(input);
+   if(!isdigit(c)){
+    return 1;
+   }
+   p->d[i][j] = c - '0';
+   j++;
+  }
+  input_skipline(input);
+  i++;
+ }
+
+ return 0;
+}
+
+int e_print(e_t e){
+ int i;
+ int j;
+ for(i=0;i<e.ni;i++){
+  for(j=0;j<e.nj;j++){
+   printf("%c", e.d[i][j] > 9 ? 'X' : '0' + e.d[i][j]);
+  }
+  printf("\n");
+ }
+ return 0;
+}
+
+int e_step(e_t *p, unsigned int *n){
+ int i;
+ int j;
+ int ni;
+ int nj;
+ int count;
+ if(n == NULL){
+  return 1;
+ }
+ n[0] = 0;
+
+ ni = p->ni;
+ nj = p->nj;
+
+ /* increasing 1 */
+ for(i=0;i<ni;i++){
+  for(j=0;j<nj;j++){
+   p->d[i][j]++;
+  }
+ }
+
+ /* flashing */
+ do{
+  count = 0;
+  for(i=0;i<ni;i++){
+   for(j=0;j<nj;j++){
+    if(p->d[i][j] != FLASHED &&
+      p->d[i][j] > 9){
+     e_incresearound(p, i, j);
+     p->d[i][j] = FLASHED;
+     count++;
+    } /* if */
+   } /* for j */
+  } /* for i */
+  n[0] += count;
+ }while(count>0);
+
+ /* setting 0 energy level for flashed */
+ for(i=0;i<ni;i++){
+  for(j=0;j<nj;j++){
+   if(p->d[i][j] == FLASHED){
+    p->d[i][j] = 0;
+   }
+  }
+ }
+
+ return 0;
+}
+
+int e_incresearound(e_t *p, int ci, int cj){
+ int i;
+ int j;
+ int i0;
+ int i1;
+ int j0;
+ int j1;
+ int ni;
+ int nj;
+
+ ni = p->ni;
+ nj = p->nj;
+
+ /* 
+  * xxx
+  * xCx
+  * xxx
+  */
+ i0 = ci>0 ? ci-1 : ci;
+ i1 = ci<ni-1 ? ci+1 : ci;
+ j0 = cj>0 ? cj-1 : cj;
+ j1 = cj<nj-1 ? cj+1 : cj;
+ for(i=i0;i<=i1;i++){
+  for(j=j0;j<=j1;j++){
+   if(i != ci || j != cj){
+    if(p->d[i][j] != FLASHED){
+     p->d[i][j]++;
+    }
+   }
+  }
+ }
 
  return 0;
 }
